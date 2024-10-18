fd6b1e474e7dcc5dcfdb21c18c7c349cb6ae0746
jas <jessyjas987@gmail.com>
2024-10-18 19:39:34 +0530

    initial

---
+++ first.js
@@ -0,0 +1,28 @@
+console.log("jasmine");
+const student={
+    fullName:"Rahul",
+
+};
+//variables
+var a=35;
+var b=16;
+console.log(a+b)
+//loops
+for(var i=0;i<5;i++)
+{
+    console.log(i)
+}
+//for each loop
+var fruits =['apple','banana','orange']
+fruits.forEach(item=>console.log(item))
+//for in 
+for(item in fruits)
+{
+    console.log(item)
+}
+var i=0;
+while(i<4)
+{
+    console.log(i);
+    i++;
+}
